e1b67ed4ae1f20c8ecdc22045527a26d31fabcc0
ehy0419 <2hy0419@naver.com>
2025-07-14 21:21:51 +0900

    Fix. arithmetic exception 0으로 나눌 때 오류

--- src/main/java/org/example/lv1/App.java
+++ src/main/java/org/example/lv1/App.java
@@ -27,24 +27,34 @@ public class App {
         // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
         char opr = scn.next().charAt(0);
 
+        int result = 0;  // result 연산에 맞는 int 선언 추가
+
         // switch 조건문 사칙연산
         switch (opr) {
             case '+':
-                System.out.println(num1 + num2);
+                result = num1 + num2;
+                // System.out.println(result); 연산 출력 추가로 표시되서 주석처리
                 break;
             case '-':    // 코드 서식 다시 지정 컨트롤+알트+L
-                System.out.println(num1 - num2);
+                result = num1 - num2;
+                // System.out.println(result); 연산 출력 추가로 표시되서 주석처리
                 break;
             case '*':
-                System.out.println(num1 * num2);
+                result = num1 * num2;
+                // System.out.println(result); 연산 출력 추가로 표시되서 주석처리
                 break;
             case '/':
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    }
-                System.out.println(num1 / num2);
+//                if (num2 == 0) {
+//                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+//                    }
+                if (num2 != 0) {    // 비교연산자 i= 를 사용해서 num2에 0이 들어올 때 false 값으로 해서 연산
+                    result = num1 / num2;  // 오류에 먼저 계산하지 않기.
+                }
+                // System.out.println(result); 연산 출력 추가로 표시되서 주석처리
                 break; // if 조건문에 입력 예외 사항 추가
+
             default:
+                System.out.println("올바른 연산 기호를 입력하세요."); // 사칙연산 기호 아닌 기호에 출력
         }
 
         // 결과값 출력
