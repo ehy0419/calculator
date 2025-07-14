092f04ea1d389677919b9828bea5d28cdc378fd3
ehy0419 <2hy0419@naver.com>
2025-07-14 19:13:43 +0900

    파일 수정

--- src/main/java/org/example/lv1/App.java
+++ src/main/java/org/example/lv1/App.java
@@ -1,4 +1,12 @@
 package org.example.lv1;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        System.out.println("두 번째 숫자를 입력하세요: ");
+    }
 }
