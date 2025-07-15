999342753f5017df705a94a09fb4814f832c3abf
ehy0419 <2hy0419@naver.com>
2025-07-15 11:00:16 +0900

    Bug. return 문 누락

--- src/main/java/org/example/lv2/App.java
+++ src/main/java/org/example/lv2/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Calculator 객체 인스턴스화
 
         while (true) {
             System.out.println("exit를 입력하면 계산은 끝");
@@ -21,13 +21,12 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
             char arithmetic = scanner.next().charAt(0);
 
-            // switch 조건문 사칙연산
-            int result = 0;  // result 연산에 맞는 int 선언 추가
+            // 연산 수행
+            Integer result = calculator.calculate(num1, num2, arithmetic);
 
-
-
-            // 결과값 출력
-            System.out.println("결과: " + result);
+            if (result != null) {
+                System.out.println("결과: " + result);
+            }
 
             // 계산 종료 시퀀스
             System.out.println("더 계산하시겠습니까? (exit를 입력하면 계산은 끝)");
--- src/main/java/org/example/lv2/Calculator.java
+++ src/main/java/org/example/lv2/Calculator.java
@@ -1,18 +1,18 @@
 package org.example.lv2;
 
 public class Calculator {
-    public Integer calculate(int num1, int num2, char arithmetic) {
+        public Integer calculate(int num1, int num2, char arithmetic) {
 
 
         // 사칙연산
-        int result;
-        // int result = 0 나눗셈 에러
+        int result = 0;
+
 
         switch (arithmetic) {
             case '+':
                 result = num1 + num2;
                 break;
-            case '-':    // 코드 서식 다시 지정 컨트롤+알트+L
+            case '-':
                 result = num1 - num2;
                 break;
             case '*':
@@ -20,7 +20,7 @@ public class Calculator {
                 break;
             case '/':
                 if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    System.out.println("나눗셈에서 0으로 나눌 수 없습니다.");
                     return null;
                 } else {
                     result = num1 / num2;
@@ -30,7 +30,5 @@ public class Calculator {
                 System.out.println("올바른 연산 기호를 입력하세요.");
                 return null;
         }
-
-        return result;
     }
 }
